2503689bbea8fe8d0ae5255d165da8b1c5a5aa43
Dinamitaa <53971920+Dinamitaa@users.noreply.github.com>
2019-08-10 08:52:57 -0400

    Add files via upload

---
+++ trabajo.py
@@ -0,0 +1,7 @@
+numero = int(input()) #convertir a string
+if numero == 1: #si usted escribe uno saldra lo siguiente:
+    print('¿Como estas?')
+elif numero == int(2): #segunda condicion
+        print('Deberias tomar algo')
+else: #si ninguna de las condiciones anteriores se cumple lo siguiente saldra en pantalla:
+    print('Esto es aburrido')
